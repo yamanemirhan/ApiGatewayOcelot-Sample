c26e0c80249f0eb743200d0fbfa5a4320138fbdb
Emirhan Yaman <yaman.emrhn@gmail.com>
2025-05-28 00:03:06 +0300

    Test in-memory caching using Ocelot

--- ApiGateway/OcelotApiGateway/Program.cs
+++ ApiGateway/OcelotApiGateway/Program.cs
@@ -1,3 +1,4 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -5,7 +6,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddOcelot(builder.Configuration).AddCacheManager(x =>
+{
+    x.WithDictionaryHandle();  // in-memory cache, use Redis
+});
 
 var app = builder.Build();
 
